8aab3e9a9aeab87f0413b0fb6c3def240acd452f
Trent McNabb <trent@Trents-MacBook-Pro.local>
2021-08-24 22:28:57 +0000

    creating tables and loading data for 2 csv

--- create_dbs.py
+++ create_dbs.py
@@ -5,10 +5,18 @@ from sqlalchemy import create_engine
 engine = create_engine(database_URI,echo=True)
 Base.metadata.create_all(engine)
 
-csv_file_path = "./data/test.csv"
+csv_file_path = "./data/vac_status_hosp_icu.csv"
 with open(csv_file_path,'r') as file:
     conn = engine.raw_connection()
     cursor = conn.cursor()
-    command = 'COPY test(id,field) FROM STDIN WITH (FORMAT CSV, HEADER TRUE)'
+    command = 'COPY vac_status_hosp_icu(date,icu_unvac,icu_partial_vac,icu_full_vac,hospitalnonicu_unvac,hospitalnonicu_partial_vac,hospitalnonicu_full_vac) FROM STDIN WITH (FORMAT CSV, HEADER TRUE)'
+    cursor.copy_expert(command,file)
+    conn.commit()
+
+csv_file_path = "./data/cases_by_vac_status.csv"
+with open(csv_file_path,'r') as file:
+    conn = engine.raw_connection()
+    cursor = conn.cursor()
+    command = 'COPY cases_by_vac_status(date,covid19_cases_unvac,covid19_cases_partial_vac,covid19_cases_full_vac,covid19_cases_vac_unknown) FROM STDIN WITH (FORMAT CSV, HEADER TRUE)'
     cursor.copy_expert(command,file)
     conn.commit()
--- models.py
+++ models.py
@@ -1,11 +1,24 @@
 import sqlalchemy
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy import Table
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class testTable(Base):
-    __tablename__ = 'test'
-    id = Column(Integer(), primary_key=True)
-    field = Column(String())
+class vac_status_hosp_icu(Base):
+    __tablename__ = 'vac_status_hosp_icu'
+    date = Column(Date(), primary_key=True)
+    icu_unvac = Column(Integer())
+    icu_partial_vac = Column(Integer())
+    icu_full_vac = Column(Integer())
+    hospitalnonicu_unvac = Column(Integer())
+    hospitalnonicu_partial_vac = Column(Integer())
+    hospitalnonicu_full_vac = Column(Integer())
+
+class cases_by_vac_status(Base):
+    __tablename__ = 'cases_by_vac_status'
+    date = Column(Date(), primary_key=True)
+    covid19_cases_unvac = Column(Integer())
+    covid19_cases_partial_vac = Column(Integer())
+    covid19_cases_full_vac = Column(Integer())
+    covid19_cases_vac_unknown = Column(Integer())
