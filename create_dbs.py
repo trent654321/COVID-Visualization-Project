3c05bb3578be93f177827625be17c94ef12d3c13
Trent McNabb <trent@Trents-MacBook-Pro.local>
2021-08-24 23:47:45 +0000

    create_dbs.py and models.py updated, now properly loading the CSV files into the database

--- create_dbs.py
+++ create_dbs.py
@@ -20,3 +20,37 @@ with open(csv_file_path,'r') as file:
     command = 'COPY cases_by_vac_status(date,covid19_cases_unvac,covid19_cases_partial_vac,covid19_cases_full_vac,covid19_cases_vac_unknown) FROM STDIN WITH (FORMAT CSV, HEADER TRUE)'
     cursor.copy_expert(command,file)
     conn.commit()
+
+csv_file_path = "./data/vaccine_doses.csv"
+with open(csv_file_path,'r') as file:
+    conn = engine.raw_connection()
+    cursor = conn.cursor()
+    command = 'COPY vaccine_doses(report_date,previous_day_total_doses_administered,previous_day_at_least_one,previous_day_fully_vaccinated,total_doses_administered,total_individuals_at_least_one,total_doses_in_fully_vaccinated_individuals,total_individuals_fully_vaccinated) FROM STDIN WITH (FORMAT CSV, HEADER TRUE)'
+    cursor.copy_expert(command,file)
+    conn.commit()
+
+
+csv_file_path = "./data/confirmed_positive_cases.csv"
+with open(csv_file_path,'r') as file:
+    conn = engine.raw_connection()
+    cursor = conn.cursor()
+    command = 'COPY confirmed_positive_cases(id,row_id,accurate_episode_date,case_reported_date,test_reported_date,specimen_date,age_group,client_gender,case_acquisition_info,outcome1,outbreak_related,reporting_phu_id,reporting_phu,reporting_phu_address,reporting_phu_city,reporting_phu_postal_code,reporting_phu_website,reporting_phu_latitude,reporting_phu_longitude) FROM STDIN WITH (FORMAT CSV, HEADER TRUE)'
+    cursor.copy_expert(command,file)
+    conn.commit()
+
+
+csv_file_path = "./data/vaccines_by_age_phu.csv"
+with open(csv_file_path,'r') as file:
+    conn = engine.raw_connection()
+    cursor = conn.cursor()
+    command = 'COPY vaccines_by_age_phu(date,phu_id,phu_name,age_group,at_least_one_dose_cumulative,second_dose_cumulative,total_population,percent_at_least_one_dose,percent_fully_vaccinated) FROM STDIN WITH (FORMAT CSV, HEADER TRUE)'
+    cursor.copy_expert(command,file)
+    conn.commit()
+
+csv_file_path = "./data/vaccines_by_age.csv"
+with open(csv_file_path,'r') as file:
+    conn = engine.raw_connection()
+    cursor = conn.cursor()
+    command = 'COPY vaccines_by_age(date,age_group,at_least_one_dose_cumulative,second_dose_cumulative,total_population,percent_at_least_one_dose,percent_fully_vaccinated) FROM STDIN WITH (FORMAT CSV, HEADER TRUE)'
+    cursor.copy_expert(command,file)
+    conn.commit()
--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 import sqlalchemy
-from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import Column, Integer, String, Date, Numeric, DateTime
 from sqlalchemy import Table
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -22,3 +22,59 @@ class cases_by_vac_status(Base):
     covid19_cases_partial_vac = Column(Integer())
     covid19_cases_full_vac = Column(Integer())
     covid19_cases_vac_unknown = Column(Integer())
+
+class vaccine_doses(Base):
+    __tablename__ = 'vaccine_doses'
+    report_date = Column(Date(), primary_key=True)
+    previous_day_total_doses_administered = Column(Integer())
+    previous_day_at_least_one = Column(Integer())
+    previous_day_fully_vaccinated = Column(Integer())
+    total_doses_administered = Column(Integer())
+    total_individuals_at_least_one = Column(Integer())
+    total_doses_in_fully_vaccinated_individuals = Column(Integer())
+    total_individuals_fully_vaccinated = Column(Integer())
+
+class confirmed_positive_cases(Base):
+    __tablename__ = 'confirmed_positive_cases'
+    id = Column(Integer(), primary_key=True)
+    row_id = Column(Integer())
+    accurate_episode_date = Column(DateTime())
+    case_reported_date = Column(DateTime())
+    test_reported_date = Column(DateTime()) 
+    specimen_date = Column(DateTime())
+    age_group = Column(String())
+    client_gender = Column(String())
+    case_acquisition_info = Column(String())
+    outcome1 = Column(String())
+    outbreak_related = Column(String())
+    reporting_phu_id = Column(Integer())
+    reporting_phu = Column(String())
+    reporting_phu_address = Column(String())
+    reporting_phu_city = Column(String())
+    reporting_phu_postal_code = Column(String())
+    reporting_phu_website = Column(String())
+    reporting_phu_latitude = Column(Numeric())
+    reporting_phu_longitude = Column(Numeric())
+
+class vaccines_by_age_phu(Base):
+    __tablename__ = 'vaccines_by_age_phu'
+    date = Column(Date(),primary_key=True)
+    phu_id = Column(Integer(),primary_key=True)
+    phu_name = Column(String())
+    age_group = Column(String(),primary_key=True)
+    at_least_one_dose_cumulative = Column(Integer())
+    second_dose_cumulative = Column(Integer())
+    total_population = Column(Integer())
+    percent_at_least_one_dose = Column(Numeric())
+    percent_fully_vaccinated = Column(Numeric())
+
+
+class vaccines_by_age(Base):
+    __tablename__ = 'vaccines_by_age'
+    date = Column(Date(),primary_key=True)
+    age_group = Column(String(),primary_key=True)
+    at_least_one_dose_cumulative = Column(Integer())
+    second_dose_cumulative = Column(Integer())
+    total_population = Column(Integer())
+    percent_at_least_one_dose = Column(Numeric())
+    percent_fully_vaccinated = Column(Numeric())
